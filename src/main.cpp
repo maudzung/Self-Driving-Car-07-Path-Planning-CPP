e078dc0c0d2e70595939cb673795375cf9a492f5
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2020-05-17 20:09:31 +0900

    feat: Using spline.h to smooth trajectories - If dis < 30: Slow down and change lane if we can

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -50,10 +51,20 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+  // Start in lane 1 (middle)
+  // Index notes: left ~ 0, middle ~ 1, right ~ 2
+  int lane = 1;
+
+  // Have a reference velocity to target (< speed limit)
+  // Initiallize with 0.0
+  double ref_vel = 0.0; //MPH
+  const double k_max_speed = 49.5;
+  const double k_max_acc = 0.224;  // ~ 5m/s^2
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
-              (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-               uWS::OpCode opCode) {
+   &map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel,&k_max_speed,&k_max_acc]
+  (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+   uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -67,8 +78,11 @@ int main() {
         string event = j[0].get<string>();
         
         if (event == "telemetry") {
+          /*
+          Get information of our car from the simulator
+          */
           // j[1] is the data JSON object
-          
+
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -84,9 +98,63 @@ int main() {
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
+          // Get number of points that were not executed by the previous trajectory
+          int prev_traj_size = previous_path_x.size();
+
+          // If there are any unprocessed points, set the "s" of the state to the last of them
+          if(prev_traj_size > 0)
+            car_s = end_path_s;
+
+          /*
+          Get information of other cars on the same side of the road from the simulator
+          */
           // Sensor Fusion Data, a list of all other cars on the same side 
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
+
+          // Set flags that indicate other cars on left/right/current lanes too close
+          bool too_close_ahead = false;
+          bool too_close_left = false;
+          bool too_close_right = false;
+
+
+          // Loop over all detected cars (in sensor_susion)
+          // sensor_fusion data format: [id, x, y, vx, vy, s, d]
+          for (int i=0; i < sensor_fusion.size(); i++) {
+            double detected_d = sensor_fusion[i][6];
+            int detected_lane = -1;
+            if ((0 < detected_d) and (detected_d < 4)) {
+              detected_lane = 0;
+            } else if ((4 < detected_d) and (detected_d < 8)) {
+              detected_lane = 1;
+            } else if ((8 < detected_d) and (detected_d < 12)) {
+              detected_lane = 2;
+            } else {
+              continue; // Ignore the detected car if it's not on the same side of the road with our car
+            }
+
+            double detected_vx = sensor_fusion[i][3];
+            double detected_vy = sensor_fusion[i][4];
+            double detected_v = sqrt(detected_vx * detected_vx + detected_vy * detected_vy);
+
+            double detected_s = sensor_fusion[i][5];
+
+            detected_s += (double)prev_traj_size * detected_v * 0.02;
+
+            // Set true if there is a car within 30m ahead in current lane
+            // or +- 30m in left/right lane
+
+            if (detected_lane == lane) {
+              too_close_ahead |= (detected_s > car_s) and (detected_s < car_s + 30.0);
+            } else if (detected_lane == (lane - 1)) {
+              too_close_left |= (detected_s > (car_s - 30.0)) and (detected_s < (car_s + 30));
+            } else if (detected_lane == (lane + 1)) {
+              too_close_right |= (detected_s > (car_s - 30.0)) and (detected_s < (car_s + 30.0));
+            }
+
+          }
+
+
 
           json msgJson;
 
@@ -94,9 +162,154 @@ int main() {
           vector<double> next_y_vals;
 
           /**
-           * TODO: define a path made up of (x,y) points that the car will visit
+           *  define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+
+          if (too_close_ahead) {
+            // Check to change lane
+            if ((!too_close_left) and lane > 0) {
+              // Our car can safely change to the left lane
+              lane--;
+            } else if ((!too_close_right) and lane < 2) {
+              // Our car can safely change to the right lane
+              lane++;
+            } else {
+              // Our car can't change lane because of dangerous
+              // Let our car slow down
+              ref_vel -= k_max_acc;
+            }
+
+          } else {
+            // There is no car ahead in the dangerous range (+30 meters)
+
+            // May change to the central lane
+            if (ref_vel < k_max_speed) {
+              ref_vel += k_max_acc;
+            }
+
+            if (ref_vel > k_max_speed)
+              ref_vel = k_max_speed;
+
+          }
+
+          /*
+          Trajectory generation, using spline libs
+          */
+          // The trajectories points
+          std::vector<double> pts_x;
+          std::vector<double> pts_y;
+
+          // Define variables to define the last car state
+          // Current state is given by car_x, car_y and car_yaw (meters and degrees)
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          if (prev_traj_size < 2) {
+            // A dummy point
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            // Add the dummy point and the current point to the trajectories points
+            pts_x.push_back(prev_car_x);
+            pts_y.push_back(prev_car_y); 
+
+            pts_x.push_back(car_x);
+            pts_y.push_back(car_y);
+          } else {
+            // Take the last two non-processed points
+            ref_x = previous_path_x[prev_traj_size-1];
+            ref_y = previous_path_y[prev_traj_size-1];
+
+            double ref_x_prev = previous_path_x[prev_traj_size-2];
+            double ref_y_prev = previous_path_y[prev_traj_size-2];
+
+            // Calculate yaw
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+            // Add the last two non-processed points to the trajectories points
+            pts_x.push_back(ref_x_prev);
+            pts_y.push_back(ref_y_prev);
+            pts_x.push_back(ref_x);
+            pts_y.push_back(ref_y);
+          }
+
+          // A variable for d values of the center of our car's next lane (if dont change lane, keep on the current lane)
+          double next_lane_d = 4*lane + 2;
+          // Add 3 far points to the trajectories points (+30m, +60m, +90m in the s axis in Frenet coordinate)
+          // convert the three points from Frenet to cartesian coordinates
+          std::vector<double> next_wp1 = getXY(car_s + 30, next_lane_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          std::vector<double> next_wp2 = getXY(car_s + 60, next_lane_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          std::vector<double> next_wp3 = getXY(car_s + 90, next_lane_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          // Push converted points
+          pts_x.push_back(next_wp1[0]);
+          pts_x.push_back(next_wp2[0]);
+          pts_x.push_back(next_wp3[0]);
+
+          pts_y.push_back(next_wp1[1]);
+          pts_y.push_back(next_wp2[1]);
+          pts_y.push_back(next_wp3[1]);
+
+          // Transform the waypoints from global coordinates to our car coordinates
+          // Shifting the car orientation to 0 degrees
+          for (int i=0; i<pts_x.size(); i++) {
+            // Computed shifted coordinates
+            double shifted_x = pts_x[i] - ref_x;
+            double shifted_y = pts_y[i] - ref_y;
+            
+            pts_x[i] = shifted_x*cos(0-ref_yaw) - shifted_y*sin(0-ref_yaw);
+            pts_y[i] = shifted_x*sin(0-ref_yaw) + shifted_y*cos(0-ref_yaw);
+          }
+
+          // Create a spline
+          tk::spline s;
+
+          // Compute spline to fit the trajectories points
+          s.set_points(pts_x, pts_y);
+
+          for (int i = 0; i<prev_traj_size; i++)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+
+          double target_x = 30.0;
+          double target_y = s(target_x); 
+          double target_dist = sqrt(target_x * target_x + target_y * target_y);
+
+          // Avoid deviding by zero
+          if (ref_vel < k_max_acc) {
+            ref_vel = k_max_acc;
+          }
+          double N = target_dist / (0.02 * ref_vel / 2.24); //2.24 to convert from MPH to m/s
+
+
+          double x_add_on = 0; // X offset to generate points
+          for (int i=1; i < 50-prev_traj_size; i++) {
+
+            // Get point using spline
+            double x_point = x_add_on + target_x / N;
+            double y_point = s(x_point);
+
+            // Update x_add_on (offset)
+            x_add_on = x_point;
+
+            // temporary variables in order not to use modified values
+            double x_point_temp = x_point;
+            double y_point_temp = y_point;
+
+            x_point = x_point_temp*cos(ref_yaw) - y_point_temp*sin(ref_yaw);
+            y_point = x_point_temp*sin(ref_yaw) + y_point_temp*cos(ref_yaw);
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
 
 
           msgJson["next_x"] = next_x_vals;
@@ -114,23 +327,23 @@ int main() {
     }  // end websocket if
   }); // end h.onMessage
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+  std::cout << "Connected!!!" << std::endl;
+});
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+ char *message, size_t length) {
+  ws.close();
+  std::cout << "Disconnected" << std::endl;
+});
 
-  int port = 4567;
-  if (h.listen(port)) {
-    std::cout << "Listening to port " << port << std::endl;
-  } else {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  
-  h.run();
+int port = 4567;
+if (h.listen(port)) {
+  std::cout << "Listening to port " << port << std::endl;
+} else {
+  std::cerr << "Failed to listen to port" << std::endl;
+  return -1;
+}
+
+h.run();
 }
